11f2d460f6347be099ffc6abe7dc8bb27613ee8a
Nexterp <dev@cbssolutions.ro>
2021-08-27 20:38:17 +0300

    required rma_reason but wihout default field

--- rma_warranty_group/models/rma.py
+++ rma_warranty_group/models/rma.py
@@ -7,8 +7,8 @@ class Rma(models.Model):
     rma_group_id = fields.Many2one(
         "rma.group", "RMA Group", index=True, ondelete="cascade"
     )
-    rma_reason_id = fields.Many2one("rma.reason", required=1, default=lambda r: r.env.ref('rma_warranty_group.before_rma_reason_required').id)
-    #1 for migrate: 1 create the before_rma_reason
+    rma_reason_id = fields.Many2one("rma.reason", required=1)
+    #1 for migrate required field in database: 1 create the before_rma_reason
     #2. make field required with default    ( because otherwise it can not put the constraint in database
     #3, take out the default 
     #,required=1,default=lambda r: r.env.ref('rma_warranty_group.before_rma_reason_required').id)
